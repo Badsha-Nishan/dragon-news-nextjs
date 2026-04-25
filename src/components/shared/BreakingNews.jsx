8197ff4df8ab45e63999d2d932d9ae01bb075931
Badsha-Nishan <badshanisan14@gmail.com>
2026-04-25 13:32:49 +0600

    added Marquee for breaking news

---
+++ src/components/shared/BreakingNews.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Marquee from "react-fast-marquee";
+
+const BreakingNews = () => {
+  const news = [
+    {
+      _id: "1",
+      title: "Breaking News: Major Event Unfolds in the City",
+    },
+    {
+      _id: "2",
+      title: "Breaking News: New Policy Announced by the Government",
+    },
+    {
+      _id: "3",
+      title: "Breaking News: Sports Team Wins Championship",
+    },
+  ];
+
+  return (
+    <div className="flex justify-center items-center gap-3 container bg-gray-200 py-4 px-3 mx-auto">
+      <button className="btn bg-red-500 text-white">Latest</button>
+      <Marquee pauseOnHover speed={80}>
+        {news.map((n) => (
+          <span className="mr-10" key={n._id}>
+            {n.title}
+          </span>
+        ))}
+      </Marquee>
+    </div>
+  );
+};
+
+export default BreakingNews;
