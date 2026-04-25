738fe38622f9243cf82d6675e5320b493b6fa8d6
Badsha-Nishan <badshanisan14@gmail.com>
2026-04-25 14:02:39 +0600

    added navbar

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
+import Image from "next/image";
+import Link from "next/link";
+import userLogo from "@/assets/user.png";
+import NavLink from "./NavLink";
 
 const Navbar = () => {
-    return (
-        <div>
-            Navbar
+  return (
+    <>
+      <div className="flex justify-between items-center gap-3 container mx-auto mt-4">
+        <div></div>
+        <ul className="flex justify-center items-center gap-5 text-gray-700">
+          <li>
+            <NavLink href={"/"}>Home</NavLink>
+          </li>
+          <li>
+            <NavLink href={"/about"}>About</NavLink>
+          </li>
+          <li>
+            <NavLink href={"/career"}>Career</NavLink>
+          </li>
+        </ul>
+        <div className="flex justify-center items-center gap-3">
+          <Image src={userLogo} width={40} height={40} alt="User Logo" />
+          <button className="btn btn-neutral">
+            <Link href={"/login"}>Login</Link>
+          </button>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Navbar;
