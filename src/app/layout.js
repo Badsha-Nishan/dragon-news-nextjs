d4bd4413c03ddd2ec7f3c62516f5b2aff2f01084
Badsha-Nishan <badshanisan14@gmail.com>
2026-04-25 12:50:39 +0600

    added dynamic date from date fns library

--- src/app/layout.js
+++ src/app/layout.js
@@ -22,10 +22,10 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
+      data-theme="light"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-      
         <main>{children}</main>
       </body>
     </html>
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import logo from "@/assets/logo.png";
+import Image from "next/image";
+import { compareAsc, format } from "date-fns";
 
 const Header = () => {
-    return (
-        <div>
-            Header
-        </div>
-    );
+  return (
+    <div className="text-center my-8 space-y-2">
+      <Image
+        className="mx-auto"
+        src={logo}
+        width={300}
+        height={200}
+        alt="Logo"
+      />
+      <p className="text-lg">Journalism Without Fear or Favour</p>
+      <p>{format(new Date(), "EEEE, MMMM dd, yyyy")}</p>
+    </div>
+  );
 };
 
 export default Header;
